fcc872ed8e93c8d11e37a1e9b0620dfa15c063a1
Yoshitaka Inoue <8393063+inoue0426@users.noreply.github.com>
2024-10-30 15:04:25 -0400

    Create drug2smiles.py

---
+++ drug2smiles.py
@@ -0,0 +1,18 @@
+import requests
+
+def get_smiles_from_compound_name(compound_name):
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{compound_name}/property/CanonicalSMILES/JSON"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        smiles = (
+            data.get("PropertyTable", {})
+            .get("Properties", [{}])[0]
+            .get("CanonicalSMILES")
+        )
+        return smiles
+    except requests.RequestException as e:
+        print(f"Error retrieving SMILES for compound name {compound_name}: {e}")
+        return None
